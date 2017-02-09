987455533520b52385f3c79894710d222a2df072
jimafisk <jimafisk@gmail.com>
2017-02-09 10:14:03 -0500

    Start theming tabs.

--- js/budget.js
+++ js/budget.js
@@ -4,13 +4,13 @@ function getJSONObject(path) {
   // Feature detection for CORS
   if ('withCredentials' in req) {
     req.open('GET', path, true);
-    // Just like regular ol' XHR
     req.onreadystatechange = function() {
       if (req.readyState === 4) {
         if (req.status >= 200 && req.status < 400) {
           //console.log(req.responseText);
-          //var obj = JSON.parse(req.responseText);
-          JSONObjCallback(req.responseText);
+          var jsonString = req.responseText;
+          var jsonObj = JSON.parse(jsonString);
+          JSONObjCallback(jsonObj);
           } else {
             // Handle error case
           }
@@ -25,14 +25,14 @@ $('.report').click(function(event) {
   getJSONObject($(this).attr('href'));
 });
 
-//var test_remote = 'http://mysafeinfo.com/api/data?list=presidents&format=jsonp';
-//var test_local = 'http://localhost:3000/test.json';
-
-//var obj = getJSONObject(test_remote);
-//var obj = getJSONObject(test_local);
-
 function JSONObjCallback(obj) {
-  console.log("value is " + obj);
-  //document.getElementById("result").innerHTML = obj.nm + ", " + obj[0].pp + ", " + obj[0].tm;
-  document.getElementById("result").innerHTML = obj;
+  $('table tbody').empty();
+  var tr;
+  for (var i = 0; i < obj.length; i++) {
+    tr = $('<tr/>');
+    tr.append("<td>" + obj[i].nm + "</td>");
+    tr.append("<td>" + obj[i].pp + "</td>");
+    tr.append("<td>" + obj[i].tm + "</td>");
+    $('table').append(tr);
+  }
 }
