824b4db7f32ee9cbbef2db8da9ac791875cfb971
jimafisk <jimafisk@gmail.com>
2017-02-16 11:41:13 -0500

    Configurable table headers.

--- js/budget.js
+++ js/budget.js
@@ -1,16 +1,22 @@
-function getJSONObject(path) {
+function getJSONObject(path, th, td) {
   var req = new XMLHttpRequest();
-
   // Feature detection for CORS
   if ('withCredentials' in req) {
     req.open('GET', path, true);
     req.onreadystatechange = function() {
       if (req.readyState === 4) {
         if (req.status >= 200 && req.status < 400) {
-          //console.log(req.responseText);
           var jsonString = req.responseText;
           var jsonObj = JSON.parse(jsonString);
-          JSONObjCallback(jsonObj);
+          var thObj = (this, th);
+          var thJSONObj = JSON.parse(thObj);
+          var tdObj = (this, td);
+          var tdJSONObj = JSON.parse(tdObj);
+          //console.log(thObj);
+          //console.log(thJSONObj);
+          //console.log(tdObj);
+          //console.log(tdJSONObj);
+          JSONObjCallback(jsonObj, thJSONObj, tdJSONObj);
           } else {
             // Handle error case
           }
@@ -22,30 +28,20 @@ function getJSONObject(path) {
 
 $('.js-table-link').click(function(event) {
   event.preventDefault();
-  getJSONObject($(this).attr('href'));
+  getJSONObject($(this).attr('href'), $(this).attr('data-js-table-th'), $(this).attr('data-js-table-td'));
   $('.js-table-title').html($(this).html());
 });
 
-function JSONObjCallback(obj) {
-  //var table = document.createElement('table');
-  //table.setAttribute("class","responsive-table responsive-table--horizontal");
-  //var thead = document.createElement('thead');
-  //var tbody = document.createElement('tbody');
-  //table.appendChild(thead);
-  //table.appendChild(tbody);
-  //$('.js-table-result').append(table);
+function JSONObjCallback(obj, th, td) {
   $('table.js-table tbody').empty();
-  //var th;
-  //th.append("<th>Name</th>");
-  //th.append("<th>Party</th>");
-  //th.append("<th>Term</th>");
-  //$('table.js-table').append(th);
   var tr;
   tr = $('<tr/>');
-  tr.append("<th>Name</th>");
-  tr.append("<th>Party</th>");
-  tr.append("<th>Term</th>");
-  $('table.js-table').append(tr);
+  for (var i = 0; i < Object.keys(th[0]).length; i++) {
+    console.log(th);
+    var j = i + 1;
+    tr.append("<th>" + th[0][j] + "</th>");
+    $('table.js-table').append(tr);
+  }
   for (var i = 0; i < obj.length; i++) {
     tr = $('<tr/>');
     tr.append("<td>" + obj[i].nm + "</td>");
