ece2dad12ae17c98c6c4b26c552280fd88f20e88
jimafisk <jimafisk@gmail.com>
2017-03-02 17:20:04 -0500

    Stub in real data.

--- js/bos-charts.js
+++ js/bos-charts.js
@@ -6,28 +6,18 @@ var myChart = new Chart(ctx, {
     datasets: [
     {
       label: 'Central Fleet Management',
-      data: [12, 19, 3, 5],
+      data: [2429952, 2672446, 2557755, 2590424],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
       ],
       borderColor: [
         'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
       ],
       borderWidth: 1
     },
     {
       label: 'Office of Streets',
-      data: [1, 4, 13, 3],
+      data: [1264046, 1360422, 1822681, 1973144],
       backgroundColor: [
         'rgba(0, 99, 132, 0.2)',
       ],
