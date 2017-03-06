6ead5f56a80f87b146a60fa43e179663e5a661b2
jimafisk <jimafisk@gmail.com>
2017-03-06 16:05:56 -0500

    Update capital projects landing page.

--- js/bos-charts.js
+++ js/bos-charts.js
@@ -4,8 +4,9 @@ var chartLabels = chartLabelsString.split(",");
 var chartDataString = document.getElementById("chart-data").getAttribute("data");
 var chartData = JSON.parse(chartDataString);
 var chartDatasets = new Array();
-var chartColors = ['rgba(9,31,47, 0.3)','rgba(252,182,26, 0.3)','rgba(40,139,228, 0.3)','rgba(251,77,66, 0.3)','rgba(243,243,243, 0.3)']
-var chartBorderColors = ['rgba(9,31,47, 1)','rgba(252,182,26, 1)','rgba(40,139,228, 1)','rgba(251,77,66, 1)','rgba(243,243,243, 1)']
+//var chartColors = ['rgba(9,31,47, 0.3)','rgba(252,182,26, 0.3)','rgba(40,139,228, 0.3)','rgba(251,77,66, 0.3)','rgba(243,243,243, 0.3)']
+var chartColors = ['rgba(9,31,47, .5)','rgba(252,182,26, .5)','rgba(40,139,228, .5)','rgba(251,77,66, .5)','rgba(183,183,183, .5)']
+var chartBorderColors = ['rgba(9,31,47, 1)','rgba(252,182,26, 1)','rgba(40,139,228, 1)','rgba(251,77,66, 1)','rgba(183,183,183, 1)']
 var k = 0;
 for (var i in chartData) {
   chartDatasets.push({
