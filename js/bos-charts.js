7959d236c51c983b1b955e81896bbd82c55a647a
jimafisk <jimafisk@gmail.com>
2017-03-07 13:00:27 -0500

    Pass basic category data to chartjs.

--- js/bos-charts.js
+++ js/bos-charts.js
@@ -2,9 +2,9 @@ var ctx = document.getElementById("myChart").getContext("2d");
 var chartLabelsString = document.getElementById("chart-labels").getAttribute("data");
 var chartLabels = chartLabelsString.split(",");
 var chartDataString = document.getElementById("chart-data").getAttribute("data");
+console.log(chartDataString);
 var chartData = JSON.parse(chartDataString);
 var chartDatasets = new Array();
-//var chartColors = ['rgba(9,31,47, 0.3)','rgba(252,182,26, 0.3)','rgba(40,139,228, 0.3)','rgba(251,77,66, 0.3)','rgba(243,243,243, 0.3)']
 var chartColors = ['rgba(9,31,47, .5)','rgba(252,182,26, .5)','rgba(40,139,228, .5)','rgba(251,77,66, .5)','rgba(183,183,183, .5)']
 var chartBorderColors = ['rgba(9,31,47, 1)','rgba(252,182,26, 1)','rgba(40,139,228, 1)','rgba(251,77,66, 1)','rgba(183,183,183, 1)']
 var k = 0;
