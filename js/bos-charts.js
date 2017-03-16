dff099effa522c3a6d6ce60f1b76ed1468458fed
jimafisk <jimafisk@gmail.com>
2017-03-16 17:23:29 -0400

    Clean up.

--- js/bos-charts.js
+++ js/bos-charts.js
@@ -1,6 +1,3 @@
-if (document.getElementById("myChart")) {
-  var ctx = document.getElementById("myChart").getContext("2d");
-}
 if (document.getElementById("totalLineChart")) {
   var totalLineChart = document.getElementById("totalLineChart").getContext("2d");
 }
@@ -11,18 +8,10 @@ if (document.getElementById("bosBarChart")) {
   var ctx2 = document.getElementById("bosBarChart").getContext("2d");
 }
 var chartLabelsString = document.getElementById("aggregateBarChart").getAttribute("data-labels");
-//var chartLabelsString = document.getElementById("chart-labels").getAttribute("data");
-//console.log(chartLabelsString);
-//var chartLabels = chartLabelsString.split(",");
 var chartLabels = chartLabelsString.replace(/^\s+|\s+$/g,"").split(/\s*,\s*/);
-console.log(chartLabels);
-//var chartDataString = document.getElementById("chart-data").getAttribute("data");
 var chartDataString = document.getElementById("aggregateBarChart").getAttribute("data");
-//console.log(chartDataString);
 var chartData = JSON.parse(chartDataString);
 var chartDatasets = new Array();
-//var chartColors = ['rgba(9,31,47, .5)','rgba(252,182,26, .5)','rgba(40,139,228, .5)','rgba(251,77,66, .5)','rgba(183,183,183, .5)'];
-//var chartBorderColors = ['rgba(9,31,47, 1)','rgba(252,182,26, 1)','rgba(40,139,228, 1)','rgba(251,77,66, 1)','rgba(183,183,183, 1)'];
 var chartColors = ['rgba(9,31,47, 1)','rgba(25,69,91, 1)','rgba(69,120,156, 1)','rgba(150,196,224, 1)'];
 var chartBorderColors = ['rgba(9,31,47, 1)','rgba(25,69,91, 1)','rgba(69,120,156, 1)','rgba(150,196,224, 1)'];
 var k = 0;
@@ -41,16 +30,6 @@ for (var i in chartData) {
   } else {
     k++;
   }
-}
-if (ctx) {
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: chartLabels,
-      datasets: chartDatasets
-    },
-    options: ""
-  });
 }
 
 if (totalLineChart) {
