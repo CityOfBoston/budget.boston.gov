983f9390b0a7ab499da49263e22fbd2221021dbf
jimafisk <jimafisk@gmail.com>
2017-03-24 16:29:07 -0400

    Move working json folders for gulp create tests.

--- _gulp/tasks/create_collections.js
+++ _gulp/tasks/create_collections.js
@@ -1,8 +1,16 @@
-var gulp          = require('gulp');
+var gulp = require('gulp');
 
 gulp.task('create_collections', function (done) {
   gulp.src(['_json/fy17/_cabinets/**'])
     .pipe(gulp.dest('_cabinets'))
   gulp.src(['_json/fy17/_departments/**'])
     .pipe(gulp.dest('_departments'))
+  gulp.src(['_json/fy17/_programs/**'])
+    .pipe(gulp.dest('_programs'))
+  gulp.src(['_json/fy17/_categories/**'])
+    .pipe(gulp.dest('_categories'))
+  gulp.src(['_json/fy17/_cp_depts/**'])
+    .pipe(gulp.dest('_cp_depts'))
+  gulp.src(['_json/fy17/_projects/**'])
+    .pipe(gulp.dest('_projects'))
 });
