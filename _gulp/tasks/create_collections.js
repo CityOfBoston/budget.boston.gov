5c98b24472a840d52b3ed20a5f1f6061dd0dd61f
jimafisk <jimafisk@gmail.com>
2017-03-24 16:17:25 -0400

    Gulp copy for departments collection.

--- _gulp/tasks/create_collections.js
+++ _gulp/tasks/create_collections.js
@@ -3,4 +3,6 @@ var gulp          = require('gulp');
 gulp.task('create_collections', function (done) {
   gulp.src(['_json/fy17/_cabinets/**'])
     .pipe(gulp.dest('_cabinets'))
+  gulp.src(['_json/fy17/_departments/**'])
+    .pipe(gulp.dest('_departments'))
 });
