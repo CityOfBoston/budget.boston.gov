b6f52a81be8f40dbb1af67fe0528ce52ad6b28fb
jimafisk <jimafisk@gmail.com>
2017-03-24 14:58:42 -0400

    Make json_wrap task wait for encoding to complete.

--- _gulp/tasks/browserSync.js
+++ _gulp/tasks/browserSync.js
@@ -4,7 +4,7 @@ var cp           = require('child_process');
 
 gulp.task('browser-sync', function() {
   browserSync.init(['css/*.css', 'js/**/*.js', 'index.html'], {
-    //open: false,
+    open: false,
     //host: '127.0.0.1',
     //proxy: '127.0.0.1',
     port: 4000,
