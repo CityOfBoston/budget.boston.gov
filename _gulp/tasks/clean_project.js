b9e614d7ca84252fdc26b61e0e86612dc3fc1f21
jimafisk <jimafisk@gmail.com>
2017-03-29 11:45:13 -0400

    Delete collections before build.

--- _gulp/tasks/clean_project.js
+++ _gulp/tasks/clean_project.js
@@ -3,4 +3,10 @@ const del = require('del');
 
 gulp.task('clean_project', function (done) {
   del.sync(['_cabinets/**']);
+  del.sync(['_departments/**']);
+  del.sync(['_programs/**']);
+  del.sync(['_categories/**']);
+  del.sync(['_cp_depts/**']);
+  del.sync(['_projects/**']);
+  return;
 });
