8c24ca02c998cd01530377e5b1c36c5829efc591
jimafisk <jimafisk@gmail.com>
2017-03-29 06:38:17 -0400

    Use Args to create collections via gulp.

--- _gulp/tasks/create_collections.js
+++ _gulp/tasks/create_collections.js
@@ -2,16 +2,17 @@ var gulp = require('gulp');
 var argv = require('yargs').argv;
 
 gulp.task('create_collections', function (done) {
-  return gulp.src(['_json/'+ argv.source  + '/_cabinets/**'])
+  gulp.src(['_source_json/' + argv.source + '/_cabinets/**'])
     .pipe(gulp.dest('_cabinets'))
-  gulp.src(['_json/fy17/_departments/**'])
+  gulp.src(['_source_json/' + argv.source + '/_departments/**'])
     .pipe(gulp.dest('_departments'))
-  gulp.src(['_json/fy17/_programs/**'])
+  gulp.src(['_source_json/' + argv.source + '/_programs/**'])
     .pipe(gulp.dest('_programs'))
-  gulp.src(['_json/fy17/_categories/**'])
+  gulp.src(['_source_json/' + argv.source + '/_categories/**'])
     .pipe(gulp.dest('_categories'))
-  gulp.src(['_json/fy17/_cp_depts/**'])
+  gulp.src(['_source_json/' + argv.source + '/_cp_depts/**'])
     .pipe(gulp.dest('_cp_depts'))
-  gulp.src(['_json/fy17/_projects/**'])
-    .pipe(gulp.dest('_projects'));
+  gulp.src(['_source_json/' + argv.source + '/_projects/**'])
+    .pipe(gulp.dest('_projects'))
+  return;
 });
