e19dc5327951b474b3dc773d6fe4e5953798c9b4
jimafisk <jimafisk@gmail.com>
2017-04-03 16:45:15 -0400

    Allow multiple bar charts on page.

--- js/bos-charts.js
+++ js/bos-charts.js
@@ -41,43 +41,53 @@ if (document.getElementById("lineChart")) {
  * Bar chart
  */
 if (document.getElementById("aggregateBarChart")) {
-  var aggregateBarChart = document.getElementById("aggregateBarChart");
-  aggregateBarChart.style.backgroundColor = '#F2F2F2';
-  aggregateBarChart.style.padding = '20px';
-  var aggregateBarChartContext = aggregateBarChart.getContext("2d");
-  var chartLabelsString = document.getElementById("aggregateBarChart").getAttribute("data-labels");
-  var chartLabels = chartLabelsString.replace(/^\s+|\s+$/g,"").split(/\s*,\s*/);
-  var chartDataString = document.getElementById("aggregateBarChart").getAttribute("data");
-  console.log(chartDataString);
-  var chartData = JSON.parse(chartDataString);
-  var chartDatasets = new Array();
-  var chartColors = ['rgba(9,31,47, 1)','rgba(25,69,91, 1)','rgba(69,120,156, 1)','rgba(150,196,224, 1)'];
-  var chartBorderColors = ['rgba(9,31,47, 1)','rgba(25,69,91, 1)','rgba(69,120,156, 1)','rgba(150,196,224, 1)'];
-  var k = 0;
-  for (var i in chartData) {
-    chartDatasets.push({
-      label: i,
-      data: chartData[i],
-      backgroundColor: chartColors[k],
-      borderColor: chartBorderColors[k],
-      borderWidth: 2,
-      pointBackgroundColor: '#232323',
-      pointBorderColor: '#232323'
-    });
-    if (k == chartColors.length -1) {
-      k = 0;
-    } else {
-      k++;
+  var barCharts = document.getElementsByClassName("barChart");
+  for (var i = 0, len = barCharts.length; i < len; i++) {
+    //var aggregateBarChart = document.getElementById("aggregateBarChart");
+    //aggregateBarChart.style.backgroundColor = '#F2F2F2';
+    //aggregateBarChart.style.padding = '20px';
+    barCharts[i].style.backgroundColor = '#F2F2F2';
+    barCharts[i].style.padding = '20px';
+    //var aggregateBarChartContext = aggregateBarChart.getContext("2d");
+    var barChartContext = barCharts[i].getContext("2d");
+    //var chartLabelsString = document.getElementById("aggregateBarChart").getAttribute("data-labels");
+    var chartLabelsString = barCharts[i].getAttribute("data-labels");
+    var chartLabels = chartLabelsString.replace(/^\s+|\s+$/g,"").split(/\s*,\s*/);
+    console.log(chartLabels);
+    //var chartDataString = document.getElementById("aggregateBarChart").getAttribute("data");
+    var chartDataString = barCharts[i].getAttribute("data");
+    console.log(chartDataString);
+    var chartData = JSON.parse(chartDataString);
+    var chartDatasets = new Array();
+    var chartColors = ['rgba(9,31,47, 1)','rgba(25,69,91, 1)','rgba(69,120,156, 1)','rgba(150,196,224, 1)'];
+    var chartBorderColors = ['rgba(9,31,47, 1)','rgba(25,69,91, 1)','rgba(69,120,156, 1)','rgba(150,196,224, 1)'];
+    var k = 0;
+    for (var j in chartData) {
+      chartDatasets.push({
+        label: j,
+        data: chartData[j],
+        backgroundColor: chartColors[k],
+        borderColor: chartBorderColors[k],
+        borderWidth: 2,
+        pointBackgroundColor: '#232323',
+        pointBorderColor: '#232323'
+      });
+      if (k == chartColors.length -1) {
+        k = 0;
+      } else {
+        k++;
+      }
     }
+    //var myChart = new Chart(aggregateBarChartContext, {
+    var myChart = new Chart(barChartContext, {
+      type: 'bar',
+      data: {
+        labels: chartLabels,
+        datasets: chartDatasets
+      },
+      options: ""
+    });
   }
-  var myChart = new Chart(aggregateBarChartContext, {
-    type: 'bar',
-    data: {
-      labels: chartLabels,
-      datasets: chartDatasets
-    },
-    options: ""
-  });
 }
 
 document.getElementById("aggregateBarChart-wrapper").style.margin = '0 0 0 15px';
