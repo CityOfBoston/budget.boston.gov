611352858ac5da51b0b715dff0369fd5f0f16e69
jimafisk <jimafisk@gmail.com>
2017-10-06 16:47:26 -0400

    Restructure source data in easier to deploy format.

--- _gulp/tasks/create_content.js
+++ _gulp/tasks/create_content.js
@@ -2,21 +2,21 @@ var gulp = require('gulp');
 var argv = require('yargs').argv;
 
 gulp.task('create_content', function (done) {
-  gulp.src(['_source/json/' + argv.source + '/_cabinets/**'])
+  gulp.src(['_source/' + argv.source + '/json/_cabinets/**'])
     .pipe(gulp.dest('_cabinets'));
-  gulp.src(['_source/json/' + argv.source + '/_departments/**'])
+  gulp.src(['_source/' + argv.source + '/json/_departments/**'])
     .pipe(gulp.dest('_departments'));
-  gulp.src(['_source/json/' + argv.source + '/_programs/**'])
+  gulp.src(['_source/' + argv.source + '/json/_programs/**'])
     .pipe(gulp.dest('_programs'));
-  gulp.src(['_source/json/' + argv.source + '/_categories/**'])
+  gulp.src(['_source/' + argv.source + '/json/_categories/**'])
     .pipe(gulp.dest('_categories'));
-  gulp.src(['_source/json/' + argv.source + '/_cp_depts/**'])
+  gulp.src(['_source/' + argv.source + '/json/_cp_depts/**'])
     .pipe(gulp.dest('_cp_depts'));
-  gulp.src(['_source/json/' + argv.source + '/_projects/**'])
+  gulp.src(['_source/' + argv.source + '/json/_projects/**'])
     .pipe(gulp.dest('_projects'));
-  gulp.src(['_source/md/' + argv.source + '/**'])
+  gulp.src(['_source/' + argv.source + '/md/**'])
     .pipe(gulp.dest('_pages'));
-  gulp.src(['_source/img/' + argv.source + '/**'])
+  gulp.src(['_source/' + argv.source + '/img/**'])
     .pipe(gulp.dest('img'));
   done();
 });
