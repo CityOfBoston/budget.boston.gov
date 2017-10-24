34dc66fd2d8cc9f30a0638058313922ff1009db4
jimafisk <jimafisk@gmail.com>
2017-10-24 13:47:18 -0400

    Update script to look for git tags.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,8 +9,31 @@
 # It requires no arguments. Example call:
 # `scripts/deploy.sh`
 
-echo "Current branch is \"${TRAVIS_BRANCH}\"."
 echo "Current tag is \"${TRAVIS_TAG}\"."
+
+# Get the directory that this script is in.
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# Create an empty array to hold _source subdirectory names.
+sources=()
+# Loop through all the directories in the _source folder.
+for directory in ${DIR}/../_source ; do
+  echo "Source: $directory found."
+  # Add directory names to sources array.
+  sources+=("$directory")
+done
+
+# Loop through names in sources array.
+for element in "${!sources}"; do
+  # Check if any of the names match the current git tag.
+	if [[ $element == ${TRAVIS_TAG} ]]; then
+	  echo "Tag ${TRAVIS_TAG} was found in _source."
+		break
+	fi
+done
+
+echo "Current branch is \"${TRAVIS_BRANCH}\"."
+
 if [[ "${TRAVIS_BRANCH}" = "develop" ]];
   then
     echo "Building with --staging flag."
