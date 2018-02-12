e1eb0cc11423e4d8eac8053a8292113f89d55b7d
jimafisk <jimafisk@gmail.com>
2018-02-12 13:23:42 -0500

    Change reserved 'source' key to 'fy_source'.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,20 +29,20 @@ parse_yaml() {
 if [[ "${TRAVIS_BRANCH}" = "develop" ]];
   then
   # for https://budget.digital-staging.boston.gov
-    echo "On ${TRAVIS_BRANCH} branch so looking for source in _config_stg.yml"
+    echo "On ${TRAVIS_BRANCH} branch so looking for FY source in _config_stg.yml"
     eval $(parse_yaml _config_stg.yml "config_")
 elif [[ "${TRAVIS_BRANCH}" = "master" ]];
   then
   # for https://budget.boston.gov
-    echo "On ${TRAVIS_BRANCH} branch so looking for source in _config.yml"
+    echo "On ${TRAVIS_BRANCH} branch so looking for FY source in _config.yml"
     eval $(parse_yaml _config.yml "config_")
 else
-  echo "Not on develop or master branches, config_source being set to 'missing'."
-  config_source="missing"
+  echo "Not on develop or master branches, config_fy_source being set to 'missing'."
+  config_fy_source="missing"
 fi
 
 # Get the source specified in appropriate config file
-echo "The source specified in config is: $config_source."
+echo "The source specified in config is: $config_fy_source."
 
 # Get the directory that this script is in.
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -64,7 +64,7 @@ source_is_found=false
 for element in $available_sources; do
   # Check if any of the names match the current git tag.
 	if [[ $element == $config_source && $element != "" ]]; then
-	  echo "Config source $config_source was found in _source."
+	  echo "Config source $config_fy_source was found in _source."
     source_is_found=true
 		break
 	fi
@@ -79,17 +79,17 @@ if ( $source_is_found ); then
   if [[ "${TRAVIS_BRANCH}" = "develop" ]];
     then
       echo "Building with --staging flag."
-      gulp build --staging --source $config_source
+      gulp build --staging --source $config_fy_source
   # for https://budget.boston.gov
   elif [[ "${TRAVIS_BRANCH}" = "master" ]];
     then
       echo "Building without environment flags."
-      gulp build --source $config_source
+      gulp build --source $config_fy_source
   else
     echo "Not develop or master branches, skipping site build."
   fi
 
 else
   # Let it be known that the source couldn't be found so the build couldn't happen.
-  echo "The source could not be determined from the config file. Skipping deploy entirely."
+  echo "The FY source could not be determined from the config file. Skipping deploy entirely."
 fi
