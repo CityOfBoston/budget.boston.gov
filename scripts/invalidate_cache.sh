c280daa8de9e4ccf92351ae3a3619e6270698aa4
jimafisk <jimafisk@gmail.com>
2018-02-14 13:11:06 -0500

    Update invalidation script to add staging env.

--- scripts/invalidate_cache.sh
+++ scripts/invalidate_cache.sh
@@ -2,13 +2,23 @@
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]
+echo "Preparing to run CDN Invalidation on $TRAVIS_BRANCH branch."
+
+if [[ "${TRAVIS_BRANCH}" = "master" ]];
 then
+  echo "Invalidating production Cloudfront."
+  # Invalidate CloudFront distribution
+  pip install --upgrade --user awscli
+  aws configure set preview.cloudfront true
+  aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_PROD_ID --paths '/*'
+elif [[ "${TRAVIS_BRANCH}" = "develop" ]];
+then
+  echo "Invalidating staging Cloudfront."
+  # Invalidate CloudFront distribution
+  pip install --upgrade --user awscli
+  aws configure set preview.cloudfront true
+  aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_STG_ID --paths '/*'
+else
   echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
   exit 0
 fi
-
-# Invalidate CloudFront distribution
-pip install --upgrade --user awscli
-aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths '/*'
